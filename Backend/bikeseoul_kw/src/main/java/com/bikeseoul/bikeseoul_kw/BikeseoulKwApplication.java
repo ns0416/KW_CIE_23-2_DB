969f8886eff6c4fa5f9da13223831cdb83216311
Nohsan Kim <ns416@namisnt.com>
2023-11-13 10:28:39 +0900

    add backend project

---
+++ Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/BikeseoulKwApplication.java
@@ -0,0 +1,13 @@
+package com.bikeseoul.bikeseoul_kw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeseoulKwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeseoulKwApplication.class, args);
+	}
+
+}
---
+++ Backend/bikeseoul_kw/src/test/java/com/bikeseoul/bikeseoul_kw/BikeseoulKwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeseoul.bikeseoul_kw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeseoulKwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
