ea10b78488cd8a28febc09ddd6b98cff06d59f37
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-25 22:20:16 +0900

    update Gift

--- Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/dao/GiftDao.java
+++ Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/dao/GiftDao.java
@@ -1,9 +1,11 @@
 package com.bikeseoul.bikeseoul_kw.dao;
 
 import com.bikeseoul.bikeseoul_kw.container.Gift;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface GiftDao {
     List<Gift> getReceivedGiftList(int receiver_id);
 
