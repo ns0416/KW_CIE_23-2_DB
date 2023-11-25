c9f0d7ff0deb9908afb57e5bfe5f73c50dab7927
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-25 22:20:35 +0900

    update Config

--- Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/dao/ConfigDao.java
+++ Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/dao/ConfigDao.java
@@ -2,10 +2,12 @@ package com.bikeseoul.bikeseoul_kw.dao;
 
 
 import com.bikeseoul.bikeseoul_kw.container.Config;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
 
+@Mapper
 public interface ConfigDao {
 	List<Map<String, Object>> getConfig();
 } 
