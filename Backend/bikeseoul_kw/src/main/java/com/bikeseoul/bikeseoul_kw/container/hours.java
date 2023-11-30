5fe7b65cb93c2f148273d63bbe94f90c23cac3bf
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-30 15:44:22 +0900

    add: ticket hours

---
+++ Backend/bikeseoul_kw/src/main/java/com/bikeseoul/bikeseoul_kw/container/hours.java
@@ -0,0 +1,15 @@
+package com.bikeseoul.bikeseoul_kw.container;
+
+public enum hours {
+    one(1), two(2)
+
+    private int value;
+
+    private hours(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
