14e0eacb117cea6162cd5b0fcbdb060aaeffdccd
HaechangLee <lcidavid@kw.ac.kr>
2023-12-09 14:52:49 +0900

    검색용 변수 추가

--- Frontend/src/pages/admin/member.js
+++ Frontend/src/pages/admin/member.js
@@ -3,21 +3,31 @@ import AdminNavbar from './AdminNavbar';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-
+import {useState} from 'react';
 
 
 function Member()
 {
+    const [queryword, setqueryword] = useState('');
+
+    function changehandler(e) {
+        setqueryword(e.target.value);
+    }
+
+    function Search(e) {
+        // axios 연동
+    }
+
+
     return (
         <>
         <AdminNavbar/>
-        <Container>
 
         <Container style={{marginTop:"30px", marginBottom: "30px"}}>
             <div class="input-group">
             <span class="input-group-text" id="basic-addon1">회원검색</span>
-                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                <button type="button" class="btn btn-primary">검색</button>
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" value={queryword} onChange={changehandler}/>
+                <button type="button" class="btn btn-primary" onClick={Search}>검색</button>
             </div>
         </Container>
 
@@ -25,35 +35,34 @@ function Member()
             <thead>
                 <tr>
                   <th>#</th>
-                  <th>id</th>
-                  <th>email</th>
-                  <th>phone</th>
-                  <th>level</th>
-                  <th>sex</th>
-                  <th>age</th>
-                  <th>is_lost</th>
-                  <th>isvalid</th>
-                  <th>created_date</th>
+                  <th>아이디</th>
+                  <th>이메일</th>
+                  <th>전화번호</th>
+                  <th>등급</th>
+                  <th>성별</th>
+                  <th>나이</th>
+                  <th>분실여부</th>
+                  <th>활성여부</th>
+                  <th>생성일자</th>
                   <th></th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>#</td>
-                    <td>id</td>
-                    <td>email</td>
-                    <td>phone</td>
-                    <td>level</td>
-                    <td>sex</td>
-                    <td>age</td>
-                    <td>is_lost</td>
-                    <td>isvalid</td>
-                    <td>created_date</td>
+                    <td>1</td>
+                    <td>example</td>
+                    <td>example@example.com</td>
+                    <td>01012345678</td>
+                    <td>99999</td>
+                    <td>남</td>
+                    <td>100</td>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>2023-10-30 03:54:44</td>
                     <td style={{textAlign:"center"}}><Button variant="primary">수정하기</Button>{' '}</td>
                 </tr>
             </tbody>
         </Table>
-        </Container>
         </>
     )
 
--- Frontend/src/pages/admin/station.js
+++ Frontend/src/pages/admin/station.js
@@ -3,21 +3,30 @@ import AdminNavbar from './AdminNavbar';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import {useState} from 'react';
 
 
 
 function Station()
 {
+    const [queryword, setqueryword] = useState('');
+
+    function changehandler(e) {
+        setqueryword(e.target.value);
+    }
+
+    function Search(e) {
+        // axios 연동
+    }
     return (
         <>
         <AdminNavbar/>
-        <Container>
 
         <Container style={{marginTop:"30px", marginBottom: "30px"}}>
             <div class="input-group">
             <span class="input-group-text" id="basic-addon1">대여소검색</span>
-                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                <button type="button" class="btn btn-primary">검색</button>
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" value={queryword} onChange={changehandler}/>
+                <button type="button" class="btn btn-primary" onClick={Search}>검색</button>
             </div>
         </Container>
 
@@ -38,18 +47,17 @@ function Station()
             <tbody>
                 <tr>
                     <td>1</td>
-                    <td>대여소 이름</td>
-                    <td>위도</td>
-                    <td>경도</td>
-                    <td>규모</td>
-                    <td>운영여부</td>
-                    <td>대여소 타입</td>
-                    <td>개설날짜</td>
+                    <td>한강한가운데</td>
+                    <td>37.539143</td>
+                    <td>127.108374</td>
+                    <td>10</td>
+                    <td>1</td>
+                    <td>lent</td>
+                    <td>2023-10-30 03:54:44</td>
                     <td style={{textAlign:"center"}}><Button variant="primary">수정하기</Button>{' '}</td>
                 </tr>
             </tbody>
         </Table>
-        </Container>
         </>
     )
 
